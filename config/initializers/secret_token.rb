f4f02731692d4dc0c8f472f7847cef89dd98b786
Hajime Furukawa <hajpoj@gmail.com>
2013-06-19 17:50:12 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FrontendLib::Application.config.secret_token = '3be132f776d5392e1981eeaa434dd506826f6b96c002185e5c3ad63be1dca57f05ad06e7d2edccd8868958999823bf21ee16e08e6614d91ae147a944b81c3ecd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+FrontendLib::Application.config.session_store :cookie_store, key: '_frontend-lib_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# FrontendLib::Application.config.session_store :active_record_store
